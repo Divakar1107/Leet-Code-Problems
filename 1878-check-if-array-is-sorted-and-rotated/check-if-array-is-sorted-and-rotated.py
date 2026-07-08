268a0731ce648806b548c79250b4dab9faf2228e
DIVAKAR R <rd1vakar2005@gmail.com>
2026-07-08 19:58:02 +0530

    Time: 3 ms (9.73%) | Memory: 19.1 MB (85.89%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -2,7 +2,7 @@ class Solution:
     def check(self, nums: List[int]) -> bool:
 
         for i in range(len(nums)):
-
+            
             rotated=nums[-i:]+nums[:-i]
 
             if rotated==sorted(rotated):
