6cc92463c02a488895dcc0fe3f1e19ac9e944474
DIVAKAR R <rd1vakar2005@gmail.com>
2026-08-22 14:20:29 +0530

    Time: 438 ms (60.03%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
-Select p.firstName, p.lastName,a.city, a.state
+/* Select p.firstName, p.lastName,a.city, a.state
 from Person as p
 left join Address as a
+on p.personId=a.personId
+*/
+
+select p.firstName, p.lastName, a.city, a.state
+from Person as p
+left join Address as a 
 on p.personId=a.personId
