fddfab68b2669146951499377fb1689b89672d58
DIVAKAR R <rd1vakar2005@gmail.com>
2026-08-22 14:41:17 +0530

    Time: 39 ms (89.44%) | Memory: 19.3 MB (31.73%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,7 +1,6 @@
 class Solution:
     def reverse(self, x: int) -> int:
-
-
+        '''
         rev=int(str(abs(x))[::-1])
 
         if x<0:
@@ -11,5 +10,17 @@ class Solution:
             return 0
 
         return rev
+'''
+        rev=int(str(abs(x))[::-1])
+
+        if x<0:
+            rev=-rev
+
+        if rev<-2**31 or rev>2**31-1:
+            return 0
+
+        return rev
+
+    
 
 
