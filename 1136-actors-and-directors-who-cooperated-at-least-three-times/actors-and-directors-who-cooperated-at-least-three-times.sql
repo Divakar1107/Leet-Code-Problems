5294358cd1f8060238b65c813dfde2472d2c3b0a
DIVAKAR R <rd1vakar2005@gmail.com>
2026-08-24 18:03:27 +0530

    Time: 479 ms (16.65%) | Memory: 0B (100.00%) - LeetSync

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
+/*
 Select actor_id, director_id
+from ActorDirector
+group by actor_id, director_id
+having count(*)>=3;
+*/
+
+Select actor_id,director_id
 from ActorDirector
 group by actor_id, director_id
 having count(*)>=3;
