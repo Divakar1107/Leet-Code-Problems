14eefb94a5cc84cdbb67eefef8b05ff609aee220
DIVAKAR R <rd1vakar2005@gmail.com>
2026-09-04 19:34:33 +0530

    Time: 336 ms (33.49%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
+/*
 Select name, population, `area`
 from World
 where `area` >=3000000 or population >=25000000;
+*/
+Select name,population,`area`
+from World
+where `area`>= 3000000 or population>=25000000
