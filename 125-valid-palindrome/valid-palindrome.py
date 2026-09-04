a5164c4d1388511a14fdfb96b423df7f44d6b15b
DIVAKAR R <rd1vakar2005@gmail.com>
2026-09-04 19:19:46 +0530

    Time: 4 ms (90.59%) | Memory: 19.6 MB (77.71%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,5 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        '''
+        cleared=""
+
+        for i in s:
+            if i.isalnum():
+                cleared+=i.lower()
+
+        return cleared==cleared[::-1]
+
+        '''
+
         cleared=""
 
         for i in s:
