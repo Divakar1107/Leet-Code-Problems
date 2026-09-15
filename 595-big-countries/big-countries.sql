201541f3bae92141d8c54a97bf3fd4c8d7beb00f
DIVAKAR R <rd1vakar2005@gmail.com>
2026-09-15 13:29:04 +0530

    Time: 373 ms (16.98%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -4,6 +4,7 @@ Select name, population, `area`
 from World
 where `area` >=3000000 or population >=25000000;
 */
-Select name,population,`area`
+
+Select name, population, `area`
 from World
-where `area`>= 3000000 or population>=25000000
+where `area`>=3000000 or population>=25000000
